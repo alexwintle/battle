a1761fc8231b6e8f5483d20489ec37b4991340a9
Alex Wintle <alexwintle663@gmail.com>
2021-10-07 15:58:32 +0100

    added web_helpers + created method to sign in and play because the code was reused constantly

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,6 @@
 feature 'Enter names' do 
   scenario 'Submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Sam'
-    click_button 'Submit'
-
-    # save_and_open_page
-    
+    sign_in_and_play
     expect(page).to have_content 'John vs. Sam'
   end
 end
