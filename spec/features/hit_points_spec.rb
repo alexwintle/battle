c01d1614ace06abfd55b0ceccc750730e9b49067
Alex Wintle <alexwintle663@gmail.com>
2021-10-07 16:00:47 +0100

    used sign_in_and_play method in the hit_points_spec file to refactor the tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Sam'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sam: 60HP'
   end
 end
