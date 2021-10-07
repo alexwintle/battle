fb2f4458f2dc66862c74c2595a510c65036f4db3
Alex Wintle <alexwintle663@gmail.com>
2021-10-07 16:14:48 +0100

    added parameters to make sign_in_and_play even more reusable by removing hardcoded names

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
-def sign_in_and_play
+def sign_in_and_play(player1, player2)
   visit('/')
-  fill_in :player_1_name, with: 'John'
-  fill_in :player_2_name, with: 'Sam'
+  fill_in :player_1_name, with: player1
+  fill_in :player_2_name, with: player2
   click_button 'Submit'
 end
